2157ef1e24321c966ab1204d1a61657080ba59f6
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-06 19:15:28 +0700

    menambahkan fungsi main untuk menguji akses nilai dari class pelajar

--- frienddua.cpp
+++ frienddua.cpp
@@ -24,3 +24,10 @@ void manusia::ShowNilaiPelajar(pelajar &a)
 {
     cout << a.nilai;
 };
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.ShowNilaiPelajar(pbudi); // akses nilai dari class pelajar
+    return 0;
+}
