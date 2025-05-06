6423d230ca4b04d57373e182ede63c1ef1446376
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-06 19:12:55 +0700

    fix eror dan membuat int main

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -3,9 +3,19 @@ using namespace std;
 
 class mahasiswa
 {
+    public:
     int nim;
     void showNim()
     {
         cout << "no induk = " << nim << endl;
     }
 };
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // pointer to object
+     mhs ->nim = 2; 
+     mhs -> showNim(); 
+     delete mhs;
+     return 0;
+
+}
